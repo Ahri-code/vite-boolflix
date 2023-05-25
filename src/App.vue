0c1d0cae12617a7ef8e8cf62c405722a123d178d
Edy Galdiolo <edygaldy302@gmail.com>
2023-05-25 17:16:06 +0200

    Template

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,38 @@
 <script>
+import HeaderApp from './components/HeaderApp.vue'
+import MainApp from './components/MainApp.vue'
+import FooterApp from './components/FooterApp.vue'
+import { store } from './data/store'
+import axios from 'axios'
 
+export default {
+    name: "App",
+    components: {
+        HeaderApp,
+        MainApp,
+        FooterApp
+    },
+    data() {
+        return {
+            store
+        }
+    }
+}
 </script>
 
-<template></template>
+<template>
+    <header>
+        <HeaderApp />
+    </header>
+    <main>
+        <MainApp />
+    </main>
+    <footer>
+        <FooterApp />
+    </footer>
+</template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use './style/specific/utility.scss' as *;
+@use './style/specific/style.scss' as *;
+</style>
